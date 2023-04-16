01b21149c81bcc4f52caac5aa69397382b21f64e
Ivan <90147664+Brossend@users.noreply.github.com>
2023-04-16 17:38:39 +0600

    Add files via upload

---
+++ programming/ООП 4/ООП 4/ООП 4.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <conio.h>
+#define SIZE 3
+
+using namespace std;
+
+class polinom
+{
+public:
+	int h = 0;
+	int* seq = new int[SIZE];
+
+	void print_result_minus()
+	{
+		cout << "Действие \"минус\"!" << endl;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			cout << i + 1 << "-й коэффициент: " << seq[i] << "\t";
+		}
+		cout << "\n" << endl;
+
+		delete[] seq;
+	}
+
+	void print_result_division()
+	{
+		cout << "Действие \"деление\"!" << endl;
+
+		for (int i = 0; i < h; i++)
+		{
+			cout << "Результат деления:\n" << h << "-й коэффициент: " << seq[i] << "\t";
+		}
+
+		cout << "\n" << endl;
+
+		delete[] seq;
+	}
+};
+
+polinom operator - (polinom a, polinom b)
+{
+	polinom sum;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum.seq[i] = a.seq[i] - b.seq[i];
+	}
+
+	return sum;
+}
+
+polinom operator / (polinom a, polinom b)
+{
+	polinom sum;
+
+	int* A = new int[SIZE];
+	sum.h;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		b.seq[i] = b.seq[i] * a.seq[0];
+		sum.seq[i] = a.seq[0];
+	}
+
+	sum.h++;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		A[i] = a.seq[i] - b.seq[i];
+	}
+
+	delete[] A;
+
+	return sum;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	polinom A, B;
+
+	cout << "______________________________________\n1-й полином!" << endl;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "Введите коэффициент " << i + 1 << "-го элемента: ";
+		cin >> A.seq[i];
+	}
+
+	cout << "\n______________________________________\n2-й полином!" << endl;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "Введите коэффициент " << i + 1 << "-го элемента: ";
+		cin >> B.seq[i];
+	}
+
+	cout << "\n______________________________________" << endl;
+
+	polinom C1 = A - B;
+
+	C1.print_result_minus();
+
+	polinom C2 = A / B;
+
+	C2.print_result_division();
+
+	_getch();
+	return 0;
+}
---
+++ programming/ООП 5/ООП 5/ООП 5.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+class Time
+{
+private:
+	int hours;
+	int minuts;
+	int seconds;
+
+	friend int time_build_up(int t, Time& value); //Дружественная функция
+public:
+	Time(int hours, int minuts, int seconds)
+	{
+		this->hours = hours;
+		this->minuts = minuts;
+		this->seconds = seconds;
+	}
+	void print_time()
+	{
+		cout << "Часы: " << hours << "\nМинуты: " << minuts << "\nСекунды: " << seconds << endl;
+	}
+};
+
+int time_build_up(int t, Time& value) //Дружественная функция
+{
+	value.seconds = value.seconds + t;
+
+	while (true)
+	{
+		if (value.seconds > 59)
+		{
+			value.minuts += 1;
+
+			value.seconds = value.seconds - 60;
+
+			if (value.minuts > 59)
+			{
+				value.hours += 1;
+				value.minuts = 0;
+			}
+			if (value.hours > 23)
+			{
+				value.hours = 0;
+			}
+			if (value.seconds <= 0)
+			{
+				value.seconds *= -1;
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	Time C1(21, 53, 12);
+
+	int t;
+
+	cout << "Введите количество секунд, на которое хотите изменить время: ";
+	cin >> t;
+
+	time_build_up(t, C1);
+
+	C1.print_time();
+
+	_getch();
+	return 0;
+}
